0a4391b89bb9600b9c7ab098ff63c28d06c9581f
bobh <bh2444151092@outlook.com>
2022-08-20 13:15:49 +0800

    finish printfmt and backtrace

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display infomration about the stack", mon_backtrace}
 };
 
 /***** Implementations of basic kernel monitor commands *****/
@@ -58,6 +59,20 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
+	uint32_t ebp = read_ebp();
+	uint32_t *p; 
+	cprintf("Stack backtrace:\n");
+	while(ebp != 0) {
+		p = (uint32_t *)ebp;
+		cprintf("ebp %08x  ", ebp);
+		cprintf("eip %08x  ", p[1]);
+		cprintf("args %08x ", p[2]);	
+		cprintf("%08x ", p[3]);
+		cprintf("%08x ", p[4]);
+		cprintf("%08x ", p[5]);
+		cprintf("%08x\n",p[6]);
+		ebp = p[0];
+	}
 	return 0;
 }
 
