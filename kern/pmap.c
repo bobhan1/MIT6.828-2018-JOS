181b5c7a459779cb606291f03729f1baf1ef1a67
bobh <bh2444151092@outlook.com>
2022-08-25 09:25:10 +0800

    tmp save for debug

--- kern/pmap.c
+++ kern/pmap.c
@@ -592,14 +592,16 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 	uintptr_t va_start = ROUNDDOWN((uintptr_t)va, PGSIZE);
 	uintptr_t va_end = ROUNDUP((uintptr_t)va + len, PGSIZE);
 	uintptr_t cva = va_start;
-	pte_t *pte;
+	pte_t *pte = NULL;
 
 	for (; cva < va_end; va += PGSIZE) {
 		struct PageInfo* pp = page_lookup(env->env_pgdir, (void *)cva, &pte);
 		if (!pp || !pte) {
+			user_mem_check_addr = cva;
 			return -E_FAULT;
 		}
 		if ((*pte & perm) != perm) {
+			user_mem_check_addr = cva;
 			return -E_FAULT;
 		}
 	}
