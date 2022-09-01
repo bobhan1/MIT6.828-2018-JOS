cabc15f9a5a1afdb081be1c36f7fb91b51194d27
bobh <bh2444151092@outlook.com>
2022-09-01 21:23:18 +0800

    finish lab5 file read/write

--- fs/fs.c
+++ fs/fs.c
@@ -148,20 +148,22 @@ file_block_walk(struct File *f, uint32_t filebno, uint32_t **ppdiskbno, bool all
 	if (filebno >= NDIRECT + NINDIRECT) {
 		return -E_INVAL;
 	}
-	if (filebno >= NDIRECT && alloc == 0) {
-		return -E_NOT_FOUND;
-	}
 	if (filebno < NDIRECT) {
 		*ppdiskbno = &f->f_direct[filebno];
 		return 0;
 	}
 	int bno;
-	if ((bno = alloc_block()) < 0) {
-		return -E_NO_DISK;
+	if (!f->f_indirect) {
+		if (alloc == 0) {
+			return -E_NOT_FOUND;
+		}
+		if ((bno = alloc_block()) < 0) {
+			return -E_NO_DISK;
+		}
+		f->f_indirect = bno;
+		memset(diskaddr(bno), 0, BLKSIZE);
 	}
-	f->f_indirect = bno;
-	memset(diskaddr(bno), 0, BLKSIZE);
-	*ppdiskbno = &(((uint32_t *)diskaddr(bno))[filebno - NDIRECT]);
+	*ppdiskbno = &(((uint32_t *)diskaddr(f->f_indirect))[filebno - NDIRECT]);
 	return 0;
 }
 
--- lib/file.c
+++ lib/file.c
@@ -141,7 +141,20 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	// remember that write is always allowed to write *fewer*
 	// bytes than requested.
 	// LAB 5: Your code here
-	panic("devfile_write not implemented");
+	// panic("devfile_write not implemented");
+	int r;
+	// if (n > PGSIZE - (sizeof(int) + sizeof(size_t))) {
+	// 	n = PGSIZE - (sizeof(int) + sizeof(size_t));
+	// }
+	assert(n <= PGSIZE - (sizeof(int) + sizeof(size_t)));
+	memmove(fsipcbuf.write.req_buf, buf, n);
+	fsipcbuf.write.req_n = n;
+	fsipcbuf.write.req_fileid = fd->fd_file.id;
+	if ((r = fsipc(FSREQ_WRITE, NULL)) < 0)
+		return r;
+	assert(r <= n);
+	assert(r <= PGSIZE - (sizeof(int) + sizeof(size_t)));
+	return r;
 }
 
 static int
