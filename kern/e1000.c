999669643dcf8a40e9effb1bf149ef7f26cf1ebc
bobh <bh2444151092@outlook.com>
2022-09-03 15:36:10 +0800

    finish lab6 transmit  initialization

--- kern/e1000.c
+++ kern/e1000.c
@@ -5,18 +5,49 @@
 
 volatile void *e1000_bar0;
 
-static void
-e1000_bar0_w(int index, int value)
+struct e1000_tx_desc tda[TDA_LEN]; // transmit descriptor array
+
+int pic_e1000_attach(struct pci_func *pcif)
 {
-	E1000_REG(index) = value;
-	E1000_REG(E1000_CTRL);  // wait for write to finish, by reading
+    pci_func_enable(pcif);
+    e1000_bar0 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("status: %08x\n", *(uint32_t *)E1000_REG(E1000_STATUS));
+
+    e1000_transmit_init();
+    return 0;
 }
 
-int 
-pic_e1000_attach(struct pci_func *pcif)
+/*
+    refer to '8254x Family of Gigabit Ethernet Controllers Software Developer’s Manual'
+    14.5 Transmit Initialization
+*/
+void e1000_transmit_init()
 {
-	pci_func_enable(pcif);
-	e1000_bar0 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-	cprintf("status: %08x\n", E1000_REG(E1000_STATUS));
-	return 0;
+    // init the Transmit Descriptor Base Address (TDBAL/TDBAH) registers
+    uint32_t *tdbal = (uint32_t *)E1000_REG(E1000_TDBAL);
+    *tdbal = PADDR(tda);
+    uint32_t *tdbah = (uint32_t *)E1000_REG(E1000_TDBAH);
+    *tdbah = 0;
+
+    // init the Transmit Descriptor Length (TDLEN) register
+    struct e1000_tdlen_desc *tdlen = (struct e1000_tdlen_desc *)E1000_REG(E1000_TDLEN);
+    tdlen->len = TDA_LEN;
+
+    // init The Transmit Descriptor Head and Tail (TDH/TDT) registers
+    uint32_t *tdh = (uint32_t *)E1000_REG(E1000_TDH);
+    *tdh = 0;
+    uint32_t *tdt = (uint32_t *)E1000_REG(E1000_TDT);
+    *tdt = 0;
+
+    // Configure the Collision Distance (TCTL.COLD)
+    struct e1000_tctl_desc *tctl = (struct e1000_tctl_desc *)E1000_REG(E1000_TCTL);
+    tctl->en = 1;
+    tctl->psp = 1;
+    tctl->ct = 0x10;
+    tctl->cold = 0x40; // for full duplex operation
+
+    struct e1000_tipg_desc *tipg = (struct e1000_tipg_desc *)E1000_REG(E1000_TIPG);
+    tipg->ipgt = 10;
+    tipg->ipgr1 = 4;
+    tipg->ipgr2 = 6;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,16 +3,67 @@
 
 #include "kern/pci.h"
 
-#define E1000_VEN_ID            0x8086 
-#define E1000_DEV_ID_82540EM    0x100E
+#define E1000_VEN_ID 0x8086
+#define E1000_DEV_ID_82540EM 0x100E
 
-#define E1000_CTRL     0x00000  /* Device Control - RW */
-#define E1000_CTRL_DUP 0x00004  /* Device Control Duplicate (Shadow) - RW */
-#define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_CTRL 0x00000     /* Device Control - RW */
+#define E1000_CTRL_DUP 0x00004 /* Device Control Duplicate (Shadow) - RW */
+#define E1000_STATUS 0x00008   /* Device Status - RO */
+#define E1000_TCTL 0x00400     /* TX Control - RW */
+#define E1000_TIPG 0x00410     /* TX Inter-packet gap -RW */
 
-#define E1000_REG(offset)    (*(volatile uint32_t *)(e1000_bar0 + offset))
+#define E1000_TDBAL 0x03800 /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH 0x03804 /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN 0x03808 /* TX Descriptor Length - RW */
+#define E1000_TDH 0x03810   /* TX Descriptor Head - RW */
+#define E1000_TDT 0x03818   /* TX Descripotr Tail - RW */
+
+#define TDA_LEN 64 /* the length of the transmit descriptor array */
+
+#define E1000_REG(offset) (void *)(e1000_bar0 + offset)
 
 int pic_e1000_attach(struct pci_func *pcif);
+void e1000_transmit_init();
 
+struct e1000_tx_desc
+{
+    uint64_t addr;
+    uint16_t length;
+    uint8_t cso;
+    uint8_t cmd;
+    uint8_t status;
+    uint8_t css;
+    uint16_t special;
+} __attribute__((packed));
 
-#endif  // SOL >= 6
+struct e1000_tdlen_desc
+{
+    uint32_t zero : 7;
+    uint32_t len : 13;
+    uint32_t rsv : 12;
+};
+
+struct e1000_tctl_desc
+{
+    uint32_t reserved1 : 1;
+    uint32_t en : 1;
+    uint32_t reserved2 : 1;
+    uint32_t psp : 1;
+    uint32_t ct : 8;
+    uint32_t cold : 10;
+    uint32_t swxoff : 1;
+    uint32_t reserved3 : 1;
+    uint32_t rtlc : 1;
+    uint32_t nrtu : 1;
+    uint32_t reserved4 : 6;
+};
+
+struct e1000_tipg_desc
+{
+    uint32_t ipgt : 10;
+    uint32_t ipgr1 : 10;
+    uint32_t ipgr2 : 10;
+    uint32_t reserved : 2;
+};
+
+#endif // SOL >= 6
